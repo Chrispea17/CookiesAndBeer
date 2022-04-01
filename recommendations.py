29eb9077d3bfaeab50a9cffd89b8333c800076cb
Christine Goodrich <christea.anna@gmail.com>
2022-04-01 03:09:25 -0400

    stuck on counting recommendations that have a rating

--- recommendations.py
+++ recommendations.py
@@ -1,5 +1,8 @@
+from numbers import Real
+
+
 class Recommendation:
-    def __init__(self, id, matchid, itemID, url):
+    def __init__(self, id: int, matchid: int, itemID: int, url: str):
 
         self.recommendationID = id
         self.uniqueUserMatchID = matchid
@@ -7,14 +10,41 @@ class Recommendation:
         self.findItem = url
         self._recommendationRating: int = None
 
-    ###NOTE SHOULD PROBABLY DECOUPLE RATING AND RECOMMENDATION IN UPDATE###
+    # Note SHOULD PROBABLY DECOUPLE RATING AND RECOMMENDATION IN UPDATE#
     # allows a recommendation to be rated
-    def setRating(self, response) -> int:
+    def setRating(self, response: str) -> int:
         ratings = {"good": 1, "bad": 0}
         self._recommendationRating = ratings[response]
-        # I don't think I need to return this return self._recommendationRating
 
 
+class Rank:
+    def __init__(self):
+        self._rank: float = None
+        self._count: int = None
+        self._sum: int = None
+
+    # Note SHOULD PROBABLY DECOUPLE COUNT AND RANK IN UPDATE#
+    # counts # of Recommendations
+    def countforRank(self, data, uniqueusermatch):
+        self._count = None
+        for item in data:
+            if (
+                item.uniqueUserMatchID == uniqueusermatch
+                and item._recommendationRating != None
+            ):
+                self._count += 1
+
+    def sumForRank(self, data, uniqueusermatch):
+        self._count = 0
+        for item in data:
+            if item.uniqueUserMatchID == uniqueusermatch:
+                self._count += 1
+
+    def setRank(self, sumratings: int, recommendationCount: int) -> float:
+        self._rank = sumratings / recommendationCount
+
+
+###NOTE need unranked people to show somewhere###
 # counts the number of recommendations from one user to another using the uniqueUserMatchID this is the first time we are looking for a psuedo repository
 # def counter(ID):
 #     countRecs = []
--- test_unit.py
+++ test_unit.py
@@ -1,31 +1,25 @@
 from pyparsing import null_debug_action
-from recommendations import Recommendation
+from recommendations import Rank, Recommendation
 import pytest
 
-# def test_ability_to_give_a_recommendation_a_positive_rating():
-#     rank = Recommendation.rating("bad")
-#     assert rank == 1
-# changed to a more accurate name for the test
-# def test_ability_to_give_a_recommendation_a_positive_rating():
 
 def test_positive_rating_equals_one():
     recommendation = Recommendation(1, 25, 1, "howdy.com")
-    recommendation._recommendationRating = recommendation.setRating("good")
+    recommendation.setRating("good")
     assert recommendation._recommendationRating == 1
 
 
-# def test_ability_to_give_recommendation_a_positive_rating():
-#     rant = Recommendation.
-
-
-def test_ability_to_give_a_recommendation_a_negative_rating():
-    rank = Recommendation.rating("bad")
-    assert rank == 0
+def test_negative_rating_equals_zero():
+    recommendation = Recommendation(1, 16, 5, "fifth.com")
+    recommendation.setRating("bad")
+    assert recommendation._recommendationRating == 0
 
 
 def test_null_rating_not_possible():
     with pytest.raises(Exception):
-        Recommendation.rating() == 1
+        recommendation = Recommendation(1, 6, 6, "garbageinput.com")
+        recommendation.setRating("garbage-in")
+        assert recommendation._recommendationRating == 1
 
 
 def test_this_is_a_recommendation_with_correct_data_types():
@@ -44,6 +38,69 @@ def test_this_is_a_recommendation_with_correct_data():
     assert recommendation.findItem == "www.findyouritem.com"
 
 
+def test_calculate_rank():
+    recommendationCount = 20
+    sumofratings = 15
+    score = Rank()
+    score.setRank(sumofratings, recommendationCount)
+    assert score._rank == 0.75
+
+
+def test_calculate_recommendation_count():
+    recList = [
+        Recommendation(1, 5, 6, "garbageinput.com"),
+        Recommendation(2, 6, 6, "garbageinput.com"),
+        Recommendation(3, 5, 6, "garbageinput.com"),
+        Recommendation(4, 5, 6, "garbageinput.com"),
+        Recommendation(5, 5, 6, "garbageinput.com")
+    ]
+    setrating = [
+        "good","bad", None, None, "good","good"
+    ]
+    for item in range(0,len(recList)-1):
+        recList[item].setRating(setRating[item])
+    counter = Rank()
+    counter.countforRank(recList, 5)
+    assert counter._count == 0
+    counter.countforRank(recList, 6)
+    assert counter._count == None
+    counter.countforRank(recList, 10)
+    assert counter._count == None
+
+# def test_calculate_recommendation_sum():
+#     recList = [
+#         Recommendation(1, 5, 6, "garbageinput.com").setRating("good"),
+#         Recommendation(2, 6, 6, "garbageinput.com").setRating("good"),
+#         Recommendation(3, 5, 6, "garbageinput.com").setRating("bad"),
+#         Recommendation(4, 5, 6, "garbageinput.com").setRating("good"),
+#         Recommendation(5, 5, 6, "garbageinput.com").setRating("bad")
+
+#     ]
+#     counter = Rank()
+#     counter.countforRank(recList, 5)
+#     assert counter._count == 4
+#     counter.countforRank(recList, 6)
+#     assert counter._count == 1
+#     counter.countforRank(recList, 10)
+#     assert counter._count == 0
+
+#     def test_calculate_unranked_sum():
+#         recList = [
+#         Recommendation(1, 5, 6, "garbageinput.com").setRating("good"),
+#         Recommendation(2, 6, 6, "garbageinput.com").setRating("good"),
+#         Recommendation(3, 5, 6, "garbageinput.com").setRating("bad"),
+#         Recommendation(4, 5, 6, "garbageinput.com").setRating("good"),
+#         Recommendation(5, 5, 6, "garbageinput.com")
+
+#     ]
+#         counter = Rank()
+#         counter.countforRank(recList, 5)
+#         assert counter._count == 4
+#         counter.countforRank(recList, 6)
+#         assert counter._count == 1
+#         counter.countforRank(recList, 10)
+#         assert counter._count == 0
+
 # didn't like the way this worked so took rating out of the initializer
 # def test_this_is_a_recommendation_with_no_rating():
 #     recommendation = Recommendation(25, 14, 4, "www.findyouritem.com")
@@ -53,11 +110,12 @@ def test_this_is_a_recommendation_with_correct_data():
 #     assert recommendation.findItem == "www.findyouritem.com"
 
 # this is the first time we are testing a repository
-def test_count_all_recommendations_given_by_User2_to_User1():
-    recommendation1 = Recommendation(25, 14, 1, "www.findyourfirstitem.com")
-    recommendation2 = Recommendation(26, 14, 2, "www.findyourseconditem.com")
-    recommendation3 = Recommendation(27, 14, 3, "www.findyourthirditem.com")
-    recommendation4 = Recommendation(28, 14, 4, "www.findyourfourthitem.com")
-    recommendation5 = Recommendation(29, 14, 5, "www.findyourfifthitem.com")
-    countRecommendations = Recommendation.counter(14)
-    assert countRecommendations == 5
+# def test_store_recommendations():
+# def test_count_all_recommendations_given_by_User2_to_User1():
+#     recommendation1 = Recommendation(25, 14, 1, "www.findyourfirstitem.com")
+#     recommendation2 = Recommendation(26, 14, 2, "www.findyourseconditem.com")
+#     recommendation3 = Recommendation(27, 14, 3, "www.findyourthirditem.com")
+#     recommendation4 = Recommendation(28, 14, 4, "www.findyourfourthitem.com")
+#     recommendation5 = Recommendation(29, 14, 5, "www.findyourfifthitem.com")
+#     countRecommendations = Recommendation.counter(14)
+#     assert countRecommendations == 5
