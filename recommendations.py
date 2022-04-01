9f9c95d41b3df299e23e211035d293cb0cbadab4
Christine Goodrich <christea.anna@gmail.com>
2022-03-31 22:58:27 -0400

    one more comment before cleanup, future will be known as cbc

--- recommendations.py
+++ recommendations.py
@@ -5,18 +5,17 @@ class Recommendation:
         self.uniqueUserMatchID = matchid
         self.itemID = itemID
         self.findItem = url
-        # self._recommendationRating = None
         self._recommendationRating: int = None
-        
+
     ###NOTE SHOULD PROBABLY DECOUPLE RATING AND RECOMMENDATION IN UPDATE###
     # allows a recommendation to be rated
     def setRating(self, response) -> int:
         ratings = {"good": 1, "bad": 0}
-        # return ratings[response]
         self._recommendationRating = ratings[response]
-        return self._recommendationRating
+        # I don't think I need to return this return self._recommendationRating
 
-    # counts the number of recommendations from one user to another using the uniqueUserMatchID this is the first time we are looking for a psuedo repository
-    # def counter(ID):
-    #     countRecs = []
-    #     for (self.recommendationID = ID)
+
+# counts the number of recommendations from one user to another using the uniqueUserMatchID this is the first time we are looking for a psuedo repository
+# def counter(ID):
+#     countRecs = []
+#     for (self.recommendationID = ID)
