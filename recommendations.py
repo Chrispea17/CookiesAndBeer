2a7ed8862934c145142b69c89c825718347a7d43
Christine Goodrich <christea.anna@gmail.com>
2022-03-31 20:42:51 -0400

    changing the way rating is stored

--- recommendations.py
+++ recommendations.py
@@ -1,4 +1,15 @@
+from xmlrpc.client import boolean
+
+
 class Recommendation:
+    def __init__(self, id: int, matchid: int, itemID: int, url: str):
+
+        self.recommendationID = id
+        self.uniqueUserMatchID = matchid
+        self.itemID = itemID
+        self.findItem = url
+        self.recommendationRating = None
+
     def rating(response):
-        rankings = {"good": 1, "bad": 0}
-        return rankings[response]
+        ratings = {"good": 1, "bad": 0}
+        return ratings[response]
--- test_unit.py
+++ test_unit.py
@@ -1,3 +1,4 @@
+from pyparsing import null_debug_action
 from recommendations import Recommendation
 import pytest
 
@@ -12,6 +13,30 @@ def test_ability_to_give_a_recommendation_a_negative_rating():
     assert rank == 0
 
 
-def test_other_rating_not_possible():
+def test_null_rating_not_possible():
     with pytest.raises(Exception):
         Recommendation.rating() == 1
+
+
+def test_this_is_a_recommendation_with_correct_data_types():
+    recommendation = Recommendation(25, 14, 4, "www.findyouritem.com")
+    assert type(recommendation.recommendationID) == int
+    assert type(recommendation.uniqueUserMatchID) == int
+    assert type(recommendation.itemID) == int
+    assert type(recommendation.findItem) == str
+
+
+def test_this_is_a_recommendation_with_correct_data():
+    recommendation = Recommendation(25, 14, 4, "www.findyouritem.com")
+    assert recommendation.recommendationID == 25
+    assert recommendation.uniqueUserMatchID == 14
+    assert recommendation.itemID == 4
+    assert recommendation.findItem == "www.findyouritem.com"
+
+
+def test_this_is_a_recommendation_with_no_rating():
+    recommendation = Recommendation(25, 14, 4, "www.findyouritem.com")
+    assert recommendation.recommendationID == 25
+    assert recommendation.uniqueUserMatchID == 14
+    assert recommendation.itemID == 4
+    assert recommendation.findItem == "www.findyouritem.com"
